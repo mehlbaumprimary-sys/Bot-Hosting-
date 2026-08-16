79be7fbd70c06aa7346a5a73e2f4735d682e25ce
mehlbaumprimary-sys <mehlbaum.primary@gmail.com>
2026-08-16 15:04:31 -0400

    Update config.js

--- bot/config.js
+++ bot/config.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 module.exports = {
     token: process.env.TOKEN,
     clientId: process.env.CLIENT_ID,
-    guildId: process.env.GUILD_ID,
     prefix: process.env.PREFIX || '~-',
     colors: {
         primary: '#5865F2',
